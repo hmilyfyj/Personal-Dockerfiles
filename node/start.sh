b069cb72fe5282fbac7ad558010e6b047fc0ea98
冯英杰 <fengit@shanjing-inc.com>
2016-04-25 14:15:41 +0800

    ad auto git pull

--- node/start.sh
+++ node/start.sh
@@ -7,5 +7,17 @@
 # Created Time: 2015/12/13
 #########################################################################
 
+Hexo_backup_url='git@git.coding.net:AIW/hexo_blog.git'
+Hexo_note_url='git@github.com:hmilyfyj/Note.git'
+
+
+#下载博客
+echo "请确认已将公钥加入仓库。"
+
+cat /root/.ssh/id_rsa.pub
+
+mkdir -p /data/blog/hexo && cd /data/blog/hexo && git init && git remote add origin $Hexo_backup_url  && yes yes | git pull origin master
+
+cd /data/blog/hexo_blog/source/_posts && git init && git remote add origin $Hexo_note_url  && && yes yes | git pull origin master
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
