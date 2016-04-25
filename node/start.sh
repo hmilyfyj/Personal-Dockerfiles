3dfe54146cfd7af049e013f3e50862761a03825b
冯英杰 <fengit@shanjing-inc.com>
2016-04-25 08:57:05 +0800

    add supervisor support

---
+++ node/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#########################################################################
+# File Name: start.sh
+# Author: Skiychan
+# Email:  dev@skiy.net
+# Version:
+# Created Time: 2015/12/13
+#########################################################################
+
+
+/usr/bin/supervisord -n -c /etc/supervisord.conf
