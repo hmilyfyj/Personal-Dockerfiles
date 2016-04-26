bb2265e07cdbd1644347789ddfc402508041596b
冯英杰 <fengit@shanjing-inc.com>
2016-04-26 16:10:27 +0800

    add personal github repo

--- node/start.sh
+++ node/start.sh
@@ -9,6 +9,7 @@
 
 Hexo_backup_url='git@git.coding.net:AIW/hexo_blog.git'
 Hexo_note_url='git@github.com:hmilyfyj/Note.git'
+Personal='git@git.coding.net:AIW/personal.git'
 
 
 #下载博客
@@ -25,5 +26,11 @@ mkdir -p /data/blog/hexo && cd /data/blog/hexo && git init && git remote add ori
 
 #cd /data/blog/hexo_blog/source/_posts && git init && git remote add origin $Hexo_note_url  && yes yes | git pull origin master
 cd /data/blog/hexo/source/_posts && git init && git remote add origin $Hexo_note_url && git pull origin master
+
+# npm install
+cd /data/blog/hexo && npm install && hexo d -g
+
+#
+mkdir -p /data/www/personal && cd /data/www/personal && git init && git remote add origin $Personal  && git pull origin master
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
