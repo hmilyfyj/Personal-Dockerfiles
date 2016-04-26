d1c9ad82bd00e111cb2880dc3bda322c7718efb1
冯英杰 <fengit@shanjing-inc.com>
2016-04-26 08:24:08 +0800

    expect

--- node/expect_git.sh
+++ node/expect_git.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/expect -f
 set timeout 60
-spawn git pull origin master
+spawn ssh -T git@git.coding.net
+expect "yes/no"
+send "yes\n"
+spawn ssh -T git@github.com
 expect "yes/no"
 send "yes\n"
 expect eof
--- node/start.sh
+++ node/start.sh
@@ -16,12 +16,13 @@ echo "请确认已将公钥加入仓库。"
 
 cat /root/.ssh/id_rsa.pub
 
+expect -f /root/expect_git.sh
 
 #mkdir -p /data/blog/hexo && cd /data/blog/hexo && git init & git remote add origin $Hexo_backup_url  &  git pull origin master
-mkdir -p /data/blog/hexo && cd /data/blog/hexo && git init & git remote add origin $Hexo_backup_url  & expect -f /root/expect_git.sh
+mkdir -p /data/blog/hexo && cd /data/blog/hexo && git init & git remote add origin $Hexo_backup_url  & git pull origin master
 
 
 #cd /data/blog/hexo_blog/source/_posts && git init && git remote add origin $Hexo_note_url  && yes yes | git pull origin master
-cd /data/blog/hexo_blog/source/_posts && git init && git remote add origin $Hexo_note_url expect -f /root/
+cd /data/blog/hexo_blog/source/_posts && git init && git remote add origin $Hexo_note_url & git pull origin master
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
